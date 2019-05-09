9fcb92bdd224e160b105e0b6e4b2b4cb7e7b773b
Ewoud Pelleriau <ewoudp@skynet.be>
2019-05-09 09:21:22 +0200

    Update Search.class.php
    Update Class search

--- classes/Search.class.php
+++ classes/Search.class.php
@@ -14,4 +14,21 @@ class Search {
         $this->search_term = $search_term;
     }
 
-    
+    public function _Search() {
+        $conn = Db::getInstance();
+        $search = $this->getSearchTerm();
+        $search = "%$search%";
+
+        $stmt = $conn->prepare("select * from posts WHERE title LIKE :s");
+        $stmt->bindValue(":s", $search);
+        $stmt->execute();
+
+        $result = array();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $result[] = $row;
+        }
+
+        return $result;
+    }
+}
